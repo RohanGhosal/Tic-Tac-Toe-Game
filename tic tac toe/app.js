bad94081567d6659fee3f94dd06c8b3380707eb9
RohanGhosal <rohanghosal401@gmail.com>
2025-01-21 12:39:08 +0530

    added draw function

--- tic tac toe/app.js
+++ tic tac toe/app.js
@@ -1,81 +1,105 @@
-// accessing elements using query selectors
+// Accessing elements using query selectors
 let boxes = document.querySelectorAll(".box");
 let restartBtn = document.querySelector(".rst-game");
 let newGamebtn = document.querySelector(".new-game");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
-//Deciding alternate turns players
+// Deciding alternate turns for players
 let turnX = true;
 
-//assigning winning patterns
+// Assigning winning patterns
 const winpatterns = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
 ];
-   
-    
-const resetGame = () =>{
-    let turnX = true;
+
+// Resetting the game
+const resetGame = () => {
+    turnX = true;
     enableBtns();
     msgContainer.classList.add("hide");
 };
 
-boxes.forEach((box)=>{
-    box.addEventListener("click",()=>{
-        if(turnX){
-            box.innerText="X";
-            turnX=false;
-        }else{
-            box.innerText="O";
-            turnX=true;
-        }
-        box.disabled = true;//for each box can be clicked once only
+// Adding click event to boxes
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (box.innerText !== "") return; // Prevent overwriting an already filled box
 
-        checkWinner ();
-        
-        
+        if (turnX) {
+            box.innerText = "X";
+            turnX = false;
+        } else {
+            box.innerText = "O";
+            turnX = true;
+        }
+
+        box.disabled = true; // Each box can be clicked only once
+
+        checkWinner();
+        if (!checkWinner() && isDraw()) {
+            drawFunction();
+        }
     });
 });
-   const disableBtns = () =>{
-    for(let box of boxes){
+
+// Disabling all buttons/boxes
+const disableBtns = () => {
+    for (let box of boxes) {
         box.disabled = true;
     }
-   }
+};
 
-   const enableBtns = () =>{
-    for(let box of boxes){
+// Enabling all buttons/boxes
+const enableBtns = () => {
+    for (let box of boxes) {
         box.disabled = false;
         box.innerText = "";
     }
-   }
-
-    const showWinner = (winner) =>{
-       msg.innerText = `Congratulations, winner is ${winner} `;
-       msgContainer.classList.remove("hide");
-       disableBtns();
-        };
-
-    const checkWinner = () => {
-        for( let pattern of winpatterns){
-          
-            let pos1val = boxes[pattern[0]].innerText;
-            let pos2val = boxes[pattern[1]].innerText;
-            let pos3val = boxes[pattern[2]].innerText;
-
-            if(pos1val != "" && pos2val != "" && pos3val != ""){
-                if(pos1val === pos2val && pos2val === pos3val){
-                    showWinner (pos1val);
-                }
-            }
-    };
 };
-newGamebtn.addEventListener("click",resetGame);
-restartBtn.addEventListener("click",resetGame);
- 
+
+// Handling draw condition
+const drawFunction = () => {
+    disableBtns();
+    msg.innerHTML = "Oops!    It's a Draw";
+    msgContainer.classList.remove("hide");
+};
+
+// Showing the winner
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, the winner is ${winner}!`;
+    msgContainer.classList.remove("hide");
+    disableBtns();
+};
+
+// Checking for a winner
+const checkWinner = () => {
+    for (let pattern of winpatterns) {
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if (pos1val !== "" && pos2val !== "" && pos3val !== "") {
+            if (pos1val === pos2val && pos2val === pos3val) {
+                showWinner(pos1val);
+                return true; // Winner found
+            }
+        }
+    }
+    return false; // No winner
+};
+
+// Checking for a draw
+const isDraw = () => {
+    return Array.from(boxes).every((box) => box.innerText !== "");
+};
+
+// Adding event listeners to reset buttons
+newGamebtn.addEventListener("click", resetGame);
+restartBtn.addEventListener("click", resetGame);
